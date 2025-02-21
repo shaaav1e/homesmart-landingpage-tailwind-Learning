89cf708b02fc44554cd3026fa0eef19f70e1d282
shaaav1e <shaaavie@gmail.com>
2025-02-21 23:04:23 +0500

    2 sections done

--- src/main.js
+++ src/main.js
@@ -2,4 +2,19 @@ import './style.css'
 
 // MOBILE NAVBAR TOGGLE
 
-const navToggle = document.getElementById('menubar');
+const navToggle = document.getElementById('menu');
+const menubar = document.getElementById('menubar');
+const icon = document.getElementById('icon');
+navToggle.addEventListener('click',()=>
+{
+menubar.classList.toggle('hidden');
+menubar.classList.toggle('flex');
+// Toggle the icon class
+if (menubar.classList.contains('hidden')) {
+    icon.classList.remove('ri-close-large-line');
+    icon.classList.add('ri-menu-3-line');
+} else {
+    icon.classList.remove('ri-menu-3-line');
+    icon.classList.add('ri-close-large-line');
+}
+});
